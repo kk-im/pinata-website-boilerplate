be9584318f229868fbfd149c1ffffc05f1950783
Kelly Kim <hi@kellykim.co>
2023-07-10 19:38:03 +0100

    Initial commit -- starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import './App.css'
-import { createHashRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 
 // Pages
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
 
 // Layout
-import Root from './components/Root'
-
-const router = createHashRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
-    </Route>
-  )
-)
 
 
 function App() {
 
   return (
-    <RouterProvider router={router} />
+  
   )
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div className="p-5 items-center">
-      <h1>This is the About page</h1>
+      
     </div>
   )
 }
